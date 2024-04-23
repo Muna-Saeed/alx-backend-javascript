1025bdd76b3e0cba0e92ae7127fd10a2fabffa61
Muna Saeed <munasaeed6367@gmail.com>
2024-04-24 00:11:13 +0300

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Using let instead of var
-  let task2 = true; // Using let instead of var
+  const task = false; // Using let instead of var
+  const task2 = true; // Using let instead of var
 
   if (trueOrFalse) {
-    let task = true; // Using let instead of var
-    let task2 = false; // Using let instead of var
+    const task = true; // Using let instead of var
+    const task2 = false; // Using let instead of var
   }
 
   return [task, task2];
