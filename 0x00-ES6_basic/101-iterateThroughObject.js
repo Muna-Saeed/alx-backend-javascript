98ece572cf3dbfbf6039cec45c9fbbf6e4c808de
Muna Saeed <munasaeed6367@gmail.com>
2024-04-24 16:18:52 +0300

    14. Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  for (const employee of reportWithIterator) {
-    result += `${employee} | `;
-  }
-  // Remove the last ' | ' separator
-  return result.slice(0, -3);
+  return reportWithIterator.join(' | ');
 }
