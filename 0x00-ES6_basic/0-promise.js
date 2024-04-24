bfa87134d1a881d6d212d72c8fc7ba22ce119430
Muna Saeed <munasaeed6367@gmail.com>
2024-04-24 23:46:54 +0300

    0. Keep every promise you make and only make promises you can keep

--- 0x00-ES6_basic/0-promise.js
+++
@@ -1,7 +0,0 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform asynchronous operation (e.g., fetch data from an API)
-    // For now, let's resolve the Promise immediately
-    resolve('Response from API');
-  });
-}
