ce964e96b0e4d87c13d887f23c61455cda5c95cc
Muna Saeed <munasaeed6367@gmail.com>
2024-04-26 23:57:51 +0300

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,19 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      } else {
-        return {
-          status: result.status,
-          value: result.reason.message,
-        };
-      }
-    });
-  });
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
