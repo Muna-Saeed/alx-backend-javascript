ca944bcac57066a2024f947e3edc947d5d5b07c1
Muna Saeed <munasaeed6367@gmail.com>
2024-04-27 02:35:32 +0300

    ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,14 @@
 class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Abstract classes cannot be instantiated directly');
-    }
     this._sqft = sqft;
   }
 
-  // Getter for sqft attribute
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method for evacuation warning message
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
 
