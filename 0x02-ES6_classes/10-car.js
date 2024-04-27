bd55f3502083f77dd03f3ef464176139b7725131
Muna Saeed <munasaeed6367@gmail.com>
2024-04-27 03:16:28 +0300

    ES6 classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,25 +1,24 @@
-const cloneKey = Symbol('cloneKey');
-
 class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-
-    // Define the cloneCar method using a symbol
-    this[cloneKey] = function() {
-      return new this.constructor(this._brand, this._motor, this._color);
-    };
   }
 
-  // Define the cloneCar method
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    if (Species) {
-      return new Species(this._brand, this._motor, this._color);
-    } else {
-      return this[cloneKey]();
-    }
+    return new Car(this._brand, this._motor, this._color);
   }
 }
 
