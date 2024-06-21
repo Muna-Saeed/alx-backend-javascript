c60ba12deb205a9eecbdf0ae8fe11f6544840336
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-21 21:25:31 +0000

    fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,17 @@
 // 1-stdin.js
+
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.exit();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+    const name = process.stdin.read();
+    if (name !== null) {
+        process.stdout.write(`Your name is: ${name.trim()}\n`);
+        process.stdin.end();
+    }
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
     console.log('This important software is now closing');
 });
