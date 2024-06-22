7d939853daad6f7a907c200dfec5e0cd5461e37d
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-22 06:15:09 +0000

    done

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Create the HTTP server
-const app = http.createServer(((req, res)) => {
+const app = http.createServer((req, res) => {
   // Set the response header content type to plain text
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   // Send the response body
@@ -10,7 +10,7 @@ const app = http.createServer(((req, res)) => {
 });
 
 // Make the server listen on port 1245
-app.listen((1245, ()) => {
+app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
 
