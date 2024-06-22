426feb1c7a178474728de514c324662daef67616
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-22 05:17:38 +0000

    done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-//process
+// process
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   process.stdout.write(`Your name is: ${input}`);
