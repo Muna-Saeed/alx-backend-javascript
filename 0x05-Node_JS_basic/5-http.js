6487546405078eb57cdfa50bc902266260bda4e4
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-22 06:22:14 +0000

    done

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,36 @@
+// a more complex HTTP server using Node's HTTP module
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(database)
+      .then((data) => {
+        res.write(data);
+        res.end();
+      })
+      .catch((err) => {
+        res.write(err.message);
+        res.end();
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
