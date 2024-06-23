58730cd9c0bc35046a37e932e384e88bc2720cae
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-06-24 01:27:46 +0300

    Update 1-calcul.tests.js

--- 0x06-unittests_in_js/1-calcul.tests.js
+++ 0x06-unittests_in_js/1-calcul.tests.js
@@ -1,37 +1,37 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
-describe('calculateNumber', function() {
-  describe('SUM', function() {
-    it('should return 6 when inputs are 1.4 and 4.5', function() {
+describe('calculateNumber', function() => {
+  describe('SUM', function() => {
+    it('should return 6 when inputs are 1.4 and 4.5', function() => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
 
-    it('should return 0 when inputs are -1.4 and 1.3', function() {
+    it('should return 0 when inputs are -1.4 and 1.3', function() => {
       assert.strictEqual(calculateNumber('SUM', -1.4, 1.3), 0);
     });
   });
 
   describe('SUBTRACT', function() {
-    it('should return -4 when inputs are 1.4 and 4.5', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
 
-    it('should return -2 when inputs are -1.4 and 0.3', function() {
+    it('should return -2 when inputs are -1.4 and 0.3', function() => {
       assert.strictEqual(calculateNumber('SUBTRACT', -1.4, 0.3), -2);
     });
   });
 
-  describe('DIVIDE', function() {
-    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+  describe('DIVIDE', function() => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
-    it('should return "Error" when inputs are 1.4 and 0', function() {
+    it('should return "Error" when inputs are 1.4 and 0', function() => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 
-    it('should return "Error" when inputs are 1.4 and 0.4', function() {
+    it('should return "Error" when inputs are 1.4 and 0.4', function() => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
     });
   });
