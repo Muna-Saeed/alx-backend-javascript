ff8ddf22192c97400661a8fc07c3de34914a8357
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-06-24 22:16:14 +0300

    Updated 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,17 +1,38 @@
-function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai.js');
 
-  if (type === 'SUM') {
-    return roundedA + roundedB;
-  } else if (type === 'SUBTRACT') {
-    return roundedA - roundedB;
-  } else if (type === 'DIVIDE') {
-    if (roundedB === 0) {
-      return 'Error';
-    }
-    return roundedA / roundedB;
-  }
-}
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
 
-module.exports = calculateNumber;
+    it('should return 0 when inputs are -1.4 and 1.3', function() {
+      expect(calculateNumber('SUM', -1.4, 1.3)).to.equal(0);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+
+    it('should return -2 when inputs are -1.4 and 0.3', function() {
+      expect(calculateNumber('SUBTRACT', -1.4, 0.3)).to.equal(-2);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when inputs are 1.4 and 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+
+    it('should return "Error" when inputs are 1.4 and 0.4', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0.4)).to.equal('Error');
+    });
+  });
+});
