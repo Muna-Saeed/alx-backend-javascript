5a95f7e0176941b19cea89eafac738c8cc1432e0
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-24 13:19:18 -0700

    4

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+// 6-payment_token.js
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+// 6-payment_token.test.js
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a successful response from the API when success is true', (done) => {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.include({ data: 'Successful response from the API' });
+      done();
+    }).catch(done);
+  });
+});
