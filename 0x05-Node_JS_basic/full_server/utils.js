7181ced998d7d108c4edb35ac739efdd36f44e5a
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-08-04 19:24:36 +0300

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
+const readDatabase = filePath => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
